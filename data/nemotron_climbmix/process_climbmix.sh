66a92fc29326f3dcb663bc153b298c9645bc68df
Shizhe Diao <sdiao@nvidia.com>
2025-10-22 21:13:34 -0700

    rename

--- data/nemotron_climbmix/process_climbmix.sh
+++ data/nemotron_climbmix/process_climbmix.sh
@@ -1,6 +1,3 @@
-# pip install -U "huggingface_hub[hf_transfer]"
-# export HF_HUB_ENABLE_HF_TRANSFER=1
-
 python download_climbmix.py
 python detokenize_climbmix.py --input_folder climbmix_small --output_folder ./
 rm -rf climbmix_small
