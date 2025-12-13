7656a7213b697828f30397bb8b9db8bdf1214231
Duoduo Yeah <lizhicuo2020@gmail.com>
2025-12-13 11:22:11 -0800

    modify dataset download url

--- nanochat/dataset.py
+++ nanochat/dataset.py
@@ -20,11 +20,11 @@ from nanochat.common import get_base_dir
 # The specifics of the current pretraining dataset
 
 # The URL on the internet where the data is hosted and downloaded from on demand
-BASE_URL = "https://huggingface.co/datasets/karpathy/fineweb-edu-100b-shuffle/resolve/main"
-MAX_SHARD = 1822 # the last datashard is shard_01822.parquet
+BASE_URL = "https://huggingface.co/datasets/duoduoyeah/tiny-story-shuffle/resolve/main"
+MAX_SHARD = 6 # the last datashard is shard_00006.parquet
 index_to_filename = lambda index: f"shard_{index:05d}.parquet" # format of the filenames
 base_dir = get_base_dir()
-DATA_DIR = os.path.join(base_dir, "base_data")
+DATA_DIR = os.path.join(base_dir, "tiny_story_data")
 os.makedirs(DATA_DIR, exist_ok=True)
 
 # -----------------------------------------------------------------------------
